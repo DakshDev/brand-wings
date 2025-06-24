13c39fa2516521df67c84bd3411449f1273d9092
bano quabil <banoquabil@gmail.com>
2025-06-24 20:40:19 +0500

    header doen

--- app.js
+++ app.js
@@ -21,8 +21,7 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = import.meta.dirname
 
 const app = express();
 
