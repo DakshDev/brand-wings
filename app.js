d2de39b518ac6a9ec5d13b006136ee97f82f96d8
lpt-krrish <krrishlaptop@lpt.com>
2025-06-26 03:40:48 +0500

    home page complete

--- app.js
+++ app.js
@@ -1,25 +1,5 @@
-// import express from "express";
-// import path from "path";
-
-
-// const app = express();
-
-// app.use(express.static(path.join(import.meta.dirname, "public")))
-// app.set('view engine', 'ejs');
-// app.set('views', './views');
-
-
-// app.get("/", (req, res) => {
-//     res.render("./views/home.html")
-// })
-
-
-// app.listen(3000, () => console.log("Live...")) 
-
-
 import express from 'express';
 import path from 'path';
-import { fileURLToPath } from 'url';
 
 const __dirname = import.meta.dirname
 
@@ -30,7 +10,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Set view engine
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+
 
 // Routes
 app.get('/', (req, res) => {
@@ -39,6 +19,10 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about'); // Renders views/about.ejs
 });
+
+
+
+
 
 // Start server
 app.listen(3000, () => console.log('Live...'));
